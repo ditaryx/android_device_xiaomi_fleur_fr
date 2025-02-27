2f21e80ed285583dbdd21647969aa21dac1facc5
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    fleur: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- device.mk
+++ device.mk
@@ -320,6 +320,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
+# UFFD GC
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.fleur
