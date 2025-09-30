66f0849a98e7dea432843d610ca3abb32e7c1b7d
Nisel <nisel11good@gmail.com>
2025-09-30 18:41:20 +0300

    fleur: Add persist.sys.activity_anim_perf_override for fix settings lag
    Requires https://github.com/AxionAOSP/android_frameworks_base/commit/f89e8fa592233d86ad2cabf81df245c4003587cb in frameworks/base

--- extract-files.py
+++ extract-files.py
@@ -54,6 +54,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
         .regex_replace('mediatek', 'mediatek-64b'),
 
+    'vendor/bin/hw/mtkfusionrild': blob_fixup()
+        .add_needed('libutils-v32.so'),
+
     'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup()
         .regex_replace('start', 'enable'),
 
